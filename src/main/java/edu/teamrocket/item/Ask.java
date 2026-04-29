98e0b9d8721cc27afcacc1359112f6b948426ee6
Pablo González <pablogonzaleztemes@gmail.com>
2026-04-29 17:57:13 +0200

    feat(Ask.java): :sparkles: add class with constructor & properties

---
+++ src/main/java/edu/teamrocket/item/Ask.java
@@ -0,0 +1,12 @@
+package edu.teamrocket.item;
+
+public class Ask implements Offer {
+    
+    private String size;
+    private Integer ask;
+
+    public Ask(String size, Integer ask) {
+        this.size = size;
+        this.ask = ask;
+    }
+}
