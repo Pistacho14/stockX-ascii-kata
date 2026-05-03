2bae7df184222400ee09174167adffdfd372534b
Pablo González <pablogonzaleztemes@gmail.com>
2026-05-04 01:49:59 +0200

    feat(Size.java): :sparkles: add new class size

---
+++ src/main/java/edu/teamrocket/criteria/Size.java
@@ -0,0 +1,29 @@
+package edu.teamrocket.criteria;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.teamrocket.item.Item;
+import edu.teamrocket.item.Offer;
+import edu.teamrocket.item.Sale;
+
+public class Size implements Criteria{
+
+    private String size;
+
+    public Size(String size) {
+        this.size = size;
+    }
+
+    @Override
+    public List<Offer> checkCriteria(Item item) {
+
+        List<Offer> sizeOffers = new ArrayList<>();
+        for (Offer offer : item.offers()) {
+            if (offer instanceof Sale && offer.size() == this.size) {
+                sizeOffers.add(offer);
+            }
+        }
+        return sizeOffers;
+    }
+}
