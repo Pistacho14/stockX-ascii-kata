69b93e0476bce16ef58cb76c548b721d95d15329
Xián Morales Fonseca <xianmorales01@gmail.com>
2026-05-03 19:08:05 +0200

    feat(Stockx): :sparkles: Add List import

--- src/main/java/edu/teamrocket/Stockx.java
+++ src/main/java/edu/teamrocket/Stockx.java
@@ -2,6 +2,7 @@ package edu.teamrocket;
 
 import edu.teamrocket.item.*;
 import edu.teamrocket.criteria.*;
+import java.util.List;
 /**
  * StockX nació en Detroit, y allí siguen haciendo
  * las cosas a la manera tradicional, 
