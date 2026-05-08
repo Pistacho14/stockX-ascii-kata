0a2045838531b0ac4335af49013fd801458c87fe
Pablo Gonzalez <pablogonzaleztemes@gmail.com>
2026-05-08 14:09:52 +0200

    feat(Bid.java): :sparkles: add compareTo

--- src/main/java/edu/teamrocket/item/Bid.java
+++ src/main/java/edu/teamrocket/item/Bid.java
@@ -21,6 +21,11 @@ public class Bid implements Offer{
     }
 
     @Override
+    public int compareTo(Offer offer) {
+        return this.bid.compareTo(offer.value());
+    }
+
+    @Override
     public String toString() {
         return "\t\t" + size + "\t" + bid + "\n";
     }
